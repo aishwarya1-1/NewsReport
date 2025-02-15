0fecc55d0e5177c16c8bb5a81f3dedbc08183799
Aishwarya Kalburgi <aishwaryakalburgi@Aishwaryas-MacBook-Air.local>
2025-02-15 23:32:37 +0530

    added provider fix3

--- crewOpenAI/agents.py
+++ crewOpenAI/agents.py
@@ -7,7 +7,7 @@ import os
 from tools import tool
 
 
-api_key = os.getenv("GEMINI_API_KEY")
+api_key = os.getenv("GOOGLE_API_KEY")
 
 if api_key:
     print(f"GEMINI_API_KEY Loaded: {'*' * (len(api_key) - 4)}{api_key[-4:]}")
@@ -15,7 +15,7 @@ else:
     print("GEMINI_API_KEY is not set!")
 
 my_llm = LLM(
-    api_key=os.getenv("GEMINI_API_KEY"),
+    api_key=os.getenv("GOOGLE_API_KEY"),
     model="gemini/gemini-1.5-flash",
 )
 #CREATING A SENIOR RESEARCHER AGENT
